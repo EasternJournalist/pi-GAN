3d57611e1c8fca2f3cd00fde1989ec1f9dd94d55
Ruicheng Wang <47034050+EasternJournalist@users.noreply.github.com>
2021-05-28 21:55:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-test_save_dir = '/output'
+test_save_dir = '/output/test'
 ckpt_dir = '/output'
 image_data_dir = '/data/52WangRuicheng/FFHQ64/image64_rescale'
 cache_dir = '/data/52WangRuicheng/FFHQ64pkl'
